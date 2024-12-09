aa5ad4cfa4fd7bff07ba497b5fe9ad09d8b98d8c
Plan Ghimire <plan.080bei028@tcioe.edu.np>
2024-12-09 14:39:38 +0545

    ............

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -24,10 +24,6 @@ import {
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
   carrent,
   jobit,
   tripguide,
@@ -51,19 +47,19 @@ export const navLinks: TNavLink[] = [
 
 const services: TService[] = [
   {
-    title: "Web Developer",
+    title: "Electrical System Designer",
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Researcher",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Robotics & Automation",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Graphics Designer",
     icon: creator,
   },
 ];
@@ -125,55 +121,56 @@ const technologies: TTechnology[] = [
 
 const experiences: TExperience[] = [
   {
-    title: "React.js Developer",
-    companyName: "Starbucks",
-    icon: starbucks,
+    title: "Electrical Engineering Internship",
+    companyName: "Nepal Manipal Teaching Hospital",
+    icon: "https://media.licdn.com/dms/image/v2/D4D0BAQFn2LprYpfaqw/company-logo_200_200/company-logo_200_200/0/1713088102200/nepalmanipal_logo?e=1741824000&v=beta&t=q9jeTfOcBVb09zhLG2RnTY180QlUpUdTNLu7IuCoSsY",
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "September 2024 - November 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Designed and analyzed comprehensive electrical systems for power distribution, lighting layouts, and biomedical equipment supply systems.",
+      "Developed and reviewed detailed Single Line Diagrams (SLDs) for the hospital and hostel, incorporating load calculations, cable sizing, and safety measures.",
+      "Observed and gained insights into low-voltage systems, including CCTV, PA systems, Ethernet, and telephone networks.",
+      "Supervised and reported on MEP works for critical units such as ICU, MOT, and Oncology.",
+      "Developed designs for biomedical equipment power systems and studied emergency systems like fire alarms, firefighting, UPS, and earthing.",
     ],
   },
   {
-    title: "React Native Developer",
-    companyName: "Tesla",
-    icon: tesla,
+    title: "Engineering Intern",
+    companyName: "Nepal Electricity Authority, Nepal",
+    icon: "https://media.licdn.com/dms/image/v2/C560BAQH-MlWDO0vKiw/company-logo_200_200/company-logo_200_200/0/1631407533564/nepal_electricity_authority_government_of_nepal_logo?e=1741824000&v=beta&t=3nv6zUw433oPjZ1cDkXewi4cYXO7D8u_6f6Qt-aN2cY",
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "Nov 2023 - Dec 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Learned the ropes of hydropower – from how turbines work to making electricity. Practical stuff that goes beyond textbooks!",
+      "Explored Nepal's electricity scene, understanding the ups and downs. It's fascinating to see how power impacts our country.",
+      "Got the lowdown on Nepal Electricity Authority (NEA) – the powerhouse behind the scenes. Explored how rules and policies shape our energy world.",
+      "More than just technical skills – this internship was about solving problems, paying attention to details, and becoming a better team player.",
     ],
   },
   {
-    title: "Web Developer",
-    companyName: "Shopify",
-    icon: shopify,
+    title: "Secretary",
+    companyName: "Electrical Generation Club (E-GEN)",
+    icon: "https://media.licdn.com/dms/image/v2/D560BAQHIHJkaT1HBDQ/company-logo_200_200/company-logo_200_200/0/1687169291342?e=1741824000&v=beta&t=S3NWErVqv55hwLAxZ212gYG04bo1caXhHu87L4JtHe8",
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "As a club secretary, I have played a crucial role in successfully organizing various programs and Training sessions.",
+      "Successfully completed ELECTRO-MELA 2.0 as a main coordinator. (ELECTRO-MELA 2.0  is biggest electrical festival in Pokhara )",
+      " I have made many improvements in this club also. ",
+      
     ],
   },
   {
-    title: "Full stack Developer",
-    companyName: "Meta",
-    icon: meta,
+    title: "President",
+    companyName: "Engineering Student Society of Tanahun and Lamjung (ESSOTL)",
+    icon: "https://media.licdn.com/dms/image/v2/D4E0BAQF3eBTjluaIig/company-logo_200_200/company-logo_200_200/0/1704103900483?e=1741824000&v=beta&t=ZibsNZ877xtdIT8mI6g_O106FaPt5VLyq3vOF-mi9Pg",
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "Apr 2023 - Jun 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "As a president, I have successfully organized various programs and Training sessions.",
+      "I have made many improvements in this Society also. ",
+
+
     ],
   },
 ];
